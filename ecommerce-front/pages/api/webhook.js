f01d3c0b360dcc3aac4a4d8c42600f0bdffbe197
Husain007 <94444107+HusainBhattiwala@users.noreply.github.com>
2023-07-05 21:37:13 +0530

    Add files via upload

---
+++ ecommerce-front/pages/api/webhook.js
@@ -0,0 +1,51 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_78889999a39c94bd3eed944c77741ae9a77aab57c2e49a3a6cdfdaa5faba51a9";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// loyal-excels-exult-deft
+//account_id = acct_1NPlPGSDxQ5kef5F
---
+++ ecommerce-front/pages/product/[id].js
@@ -0,0 +1,77 @@
+import Button from "@/components/Button";
+import { CartContext } from "@/components/CartContext";
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import ProductImages from "@/components/ProductImages";
+import Title from "@/components/Title";
+import WhiteBox from "@/components/WhiteBox";
+import CartIcon from "@/components/icons/CartIcon";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import { useContext } from "react";
+import { styled } from "styled-components";
+
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 0.8fr 1.2fr;
+  }
+  gap: 40px;
+  margin: 40px 0;
+`;
+
+const Desc = styled.p`
+  color: #282828;
+  font-weight: 400;
+`;
+
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+
+const Price = styled.span`
+  font-size: 1.4rem;
+`;
+
+export default function ProductPage({ product }) {
+  const { addProducts } = useContext(CartContext);
+  return (
+    <>
+      <Header />
+      <Center>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <Desc>{product.description}</Desc>
+            <PriceRow>
+              <div>
+                <Price>₹{product.price}</Price>
+              </div>
+              <Button onClick={() => addProducts(product._id)} primary>
+                <CartIcon />
+                Add to cart
+              </Button>
+            </PriceRow>
+          </div>
+        </ColWrapper>
+      </Center>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  await mongooseConnect();
+  const { id } = context.query;
+  const product = await Product.findById(id);
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
+}
